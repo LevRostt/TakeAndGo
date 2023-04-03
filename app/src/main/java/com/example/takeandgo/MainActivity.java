a35deeccf4565b2235a0471d8ee220f40eef7329
LevRost <levitskiy.r.l@edu.mirea.ru>
2023-04-03 19:14:13 +0300

    Layouts profileScreen, some correction on other, Login firebase logic(useless. my bad).

--- app/src/main/java/com/example/takeandgo/MainActivity.java
+++ app/src/main/java/com/example/takeandgo/MainActivity.java
@@ -3,19 +3,40 @@ package com.example.takeandgo;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
+
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.takeandgo.databinding.ActivityMainBinding;
 
 
 public class MainActivity extends FragmentActivity {
 
+    private FragmentManager fragmentManager;
     private ActivityMainBinding mBinding;
+    private FragmentTransaction fragmentTransaction;
+
+    public void onNavigationFragmentSelector(String name){
+
+        fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.replace(R.id.main_fragment, RegisterFragment.class, null);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(mBinding.getRoot());
+        fragmentManager = getSupportFragmentManager();
+
+
     }
 }
--- app/src/main/java/com/example/takeandgo/RegisterFragment.java
+++ app/src/main/java/com/example/takeandgo/RegisterFragment.java
@@ -5,53 +5,38 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
-import com.example.takeandgo.databinding.LoginFragmentBinding;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
+import com.example.takeandgo.databinding.RegisterFragmentBinding;
 import com.google.firebase.auth.FirebaseAuth;
 
-import java.util.Objects;
 
-
-public class LoginFragment extends Fragment {
-    private LoginFragmentBinding mBinding;
-    public final static String fragmentName = "loginFragment";
+public class RegisterFragment extends Fragment {
+    private RegisterFragmentBinding mBinding;
+    public final static String fragmentName = "registerFragment";
     private FirebaseAuth mAuth;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState){
-        mBinding = LoginFragmentBinding.inflate(inflater, container, false);
-
-        mBinding.regButton.setOnClickListener(view -> {
-
-            ((MainActivity) getActivity()).onNavigationFragmentSelector(RegisterFragment.fragmentName);
-
-        });
+        mBinding = RegisterFragmentBinding.inflate(inflater, container, false);
 
         mAuth = FirebaseAuth.getInstance();
-        mBinding.loginButton.setOnClickListener(view ->{
+        mBinding.regButton.setOnClickListener(view ->{
 
             if (mBinding.loginEdittext.getText().toString().isEmpty() || mBinding.passwordEdittext.getText().toString().isEmpty()){
                 Log.d("My_Auth_Command", "Empty");
             } else{
                 Log.d("My_Auth_Command_text", mBinding.loginEdittext.getText().toString() + " " + mBinding.passwordEdittext.getText().toString());
-                mAuth.signInWithEmailAndPassword(mBinding.loginEdittext.getText().toString(), mBinding.passwordEdittext.getText().toString())
+                mAuth.createUserWithEmailAndPassword(mBinding.loginEdittext.getText().toString(), mBinding.passwordEdittext.getText().toString())
                         .addOnCompleteListener(task -> {
                             if (task.isSuccessful()){
-                                Log.d("My_Auth_Command", "Auth is done!");
+                                Log.d("My_Auth_Command", "Adding is done!");
                                 Log.d("My_Auth_Command_UID",  mAuth.getUid());
                             }
                             else{
-                                Log.d("My_Auth_Command", "Auth doesn't done!");
+                                Log.d("My_Auth_Command", "Adding doesn't done!");
                             }
                         });
             }
